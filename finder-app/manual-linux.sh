21c69a68eb951ccd8114bd2eddfbeb444c3b1664
halmaden <Haluk.Madencioglu@colorado.edu>
2024-10-04 17:58:12 -0400

    working assig3-part2 first upload

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,18 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    echo "running make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mproper #   deep clean"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper #   deep clean
+    echo "running make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig # default configuration for VIRT"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig # default configuration for VIRT
+    echo "running make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all # compile the kernel"
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all # compile the kernel
+    
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
+
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +57,14 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir ${OUTDIR}/rootfs
+cd rootfs # the last cd was to ${OUTDIR}  
+SYSROOT=`aarch64-none-linux-gnu-gcc --print-sysroot`
+
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin/usr/lib/usr/sbin/var/log home/conf
+
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +73,62 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
+        
+    
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=../rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
+
+cd "$OUTDIR"/rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cd "$OUTDIR"
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 rootfs/lib/
+cp ${SYSROOT}/lib64/libm.so.6  rootfs/lib64/
+cp ${SYSROOT}/lib64/libc.so.6  rootfs/lib64/
+cp ${SYSROOT}/lib64/libresolv.so.2  rootfs/lib64/
+
 
 # TODO: Make device nodes
+cd rootfs
+sudo mknod -m 666 dev/null  c 1 3 
+sudo mknod -m 600 dev/console  c 5 1  
 
 # TODO: Clean and build the writer utility
 
+
+cd $"$OUTDIR"/rootfs/home
+cp "$FINDER_APP_DIR"/writer.c ./
+cp "$FINDER_APP_DIR"/Makefile ./
+make CROSS_COMPILE=${CROSS_COMPILE}
+
 # TODO: Copy the finder related scripts and executables to the /home directory
+
+# we're still in ${OUTDIR}/rootfs/home
+cp "$FINDER_APP_DIR"/finder.sh ./ 
+cp "$FINDER_APP_DIR"/finder-test.sh ./
+cp "$FINDER_APP_DIR"/conf/username.txt ./conf/
+cp "$FINDER_APP_DIR"/conf/assignment.txt ./conf/
+cp "$FINDER_APP_DIR"/autorun-qemu.sh ./
+
 # on the target rootfs
 
+cd "$OUTDIR"/rootfs
+
 # TODO: Chown the root directory
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd "$OUTDIR"
+gzip -f initramfs.cpio
